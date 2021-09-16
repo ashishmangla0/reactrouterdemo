30a5296698144e5c99bb5bb9e97e3fd792e7889b
ashish <ashishmangla1991@gmail.com>
2021-09-16 12:18:11 +0530

    a

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,7 +4,7 @@ import './Navigation.css';
 
 const Navigation = (props) => (
   <div id="myNav" className={`overlay ${props.opennav ? 'opennav' : 'closenav'}`}>
-    <a className="closebtn" onClick={() => props.closeNav()} >&times;</a>
+    <span className="closebtn" onClick={() => props.closeNav()} >&times;</span>
     <div className="overlay-content">
       <Link to="/" onClick={() => props.closeNav()}>Home</Link>
       <Link to="/vitamin" onClick={() => props.closeNav()}>Vitamin Juice</Link>
